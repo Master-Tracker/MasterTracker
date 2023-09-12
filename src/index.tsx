25960e13ad6415090f915db38b2eb8a4a9df8a3c
unknown <yanceevillanueva16@gmail.com>
2023-09-12 23:57:49 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { Main } from './features/pages';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
